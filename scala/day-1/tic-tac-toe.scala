0dff944b6dac3e0a028746b15ebb58164abeab62
John Brunton <john_brunton@hotmail.co.uk>
2015-02-25 23:17:00 +0000

    Scala tic tac toe checker

---
+++ scala/day-1/tic-tac-toe.scala
@@ -0,0 +1,70 @@
+object Cell {
+  val VALID_VALUES = Set("X", "O", " ")
+}
+
+class Cell(val value: String) {
+  if (!Cell.VALID_VALUES(value)) {
+    throw new Exception("Invalid cell value: " + value)
+  }
+
+  override def equals(other: Any):Boolean = other match {
+    case other: Cell => this.value == other.value
+    case _ => false
+  }
+}
+
+object Line {
+  val CELL_COUNT = 3;
+  def from(cells: List[Cell]): Line = {
+    new Line(cells)
+  }
+}
+
+class Line(val cells: List[Cell]) {
+  if (cells.length != Line.CELL_COUNT) {
+    throw new Exception("Invalid line length: " + cells.length)
+  }
+  
+  def isWinner(player: String): Boolean = {
+    cells.forall(x => x.value == player)
+  }
+}
+
+object Board {
+  val ROW_COUNT = 3;
+}
+
+class Board(val rows: List[Line]) {
+  if (rows.length != Board.ROW_COUNT) {
+    throw new Exception("Invalid board size: " + rows.length)
+  }
+  
+  def columns: List[Line] = {
+    List(0,1,2).map(k => {
+      new Line(rows.map(x => x.cells(k)))
+    })
+  }
+  
+  def diagonals: List[Line] = {
+    List(
+      new Line(List(rows(0).cells(0), rows(1).cells(1), rows(2).cells(2))),
+      new Line(List(rows(0).cells(2), rows(1).cells(1), rows(2).cells(0)))
+    )
+  }
+  
+  def isWinner(player: String): Boolean = {
+    val lines = rows ++ columns ++ diagonals
+    lines.exists(x => x.isWinner(player))
+  }
+}
+
+val board = new Board(
+  List(
+    new Line(List(new Cell(" "), new Cell("O"), new Cell("X"))),
+    new Line(List(new Cell("O"), new Cell("X"), new Cell("O"))),
+    new Line(List(new Cell("X"), new Cell("O"), new Cell("X")))
+  )
+)
+
+println(board.isWinner("X"))
+println(board.isWinner("O"))
