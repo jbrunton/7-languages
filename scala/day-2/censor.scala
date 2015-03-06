474616d3e71a0378398a1519c16abd08f5eb1665
John Brunton <john_brunton@hotmail.co.uk>
2015-03-06 20:28:29 +0000

    load censor rules from file

--- scala/day-2/censor.scala
+++ scala/day-2/censor.scala
@@ -1,10 +1,24 @@
+import scala.io.Source
+
 trait Censor {
-  val words = Map("Shoot" -> "Pucky", "Darn" -> "Beans")
+  val words = loadWords("words.txt")
 
   def censor(text: String): String = {
     (text /: words) {
       (str, word) => str.replaceAll(word._1, word._2)
     }
+  }
+  
+  private def loadWords(filename: String) = {
+    val rule = """\"(.*)\"\s*->\s*\"(.*)\"""".r
+    Source.fromFile(filename).getLines().map {
+      line => {
+        line match {
+          case rule(w1, w2) => (w1, w2)
+          case _ => throw new Exception("Invalid syntax.")
+        }
+      }
+    }.toMap
   }
 }
 
