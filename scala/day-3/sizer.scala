0dbd13a1c2ec04fdbed20aa059e003d3c2cb4581
John Brunton <john_brunton@hotmail.co.uk>
2015-03-07 11:10:28 +0000

    parse html with tagsoup

--- scala/day-3/sizer.scala
+++ scala/day-3/sizer.scala
@@ -1,56 +1,67 @@
 import scala.io._
 import scala.actors._
 import Actor._
+import scala.xml.XML
+import scala.xml.parsing.NoBindingFactoryAdapter
+import org.xml.sax.InputSource
+import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 
-// START:loader
 object PageLoader {
- def getPageSize(url : String) = Source.fromURL(url).mkString.length
+  def getPageStats(url : String) = {
+    val length = Source.fromURL(url).mkString.length
+    val links = (loadPage(url) \\ "a").toList.length
+    (length, links)
+  }
+
+  def loadPage(url: String) = {
+    val parser = (new SAXFactoryImpl).newSAXParser()
+    val source = new InputSource(url)
+    val adapter = new NoBindingFactoryAdapter
+    adapter.loadXML(source, parser)
+  }
 }
-// END:loader
 
 val urls = List("http://www.amazon.com/", 
                "http://www.twitter.com/",
                "http://www.google.com/",
                "http://www.cnn.com/" )
 
-// START:time
 def timeMethod(method: () => Unit) = {
  val start = System.nanoTime
  method()
  val end = System.nanoTime
  println("Method took " + (end - start)/1000000000.0 + " seconds.")
 }
-// END:time
 
-// START:sequential
 def getPageSizeSequentially() = {
  for(url <- urls) {
-   println("Size for " + url + ": " + PageLoader.getPageSize(url))
+   val (size, links) = PageLoader.getPageStats(url)
+   println("Stats for " + url + ":")
+   println("  size  = "  + size)
+   println("  links = " + links)
  }
 }
-// END:sequential
 
-// START:concurrent
 def getPageSizeConcurrently() = {
  val caller = self
 
  for(url <- urls) {
-   actor { caller ! (url, PageLoader.getPageSize(url)) }
+   actor { caller ! (url, PageLoader.getPageStats(url)) }
  }
 
  for(i <- 1 to urls.size) {
    receive {
-     case (url, size) =>
-       println("Size for " + url + ": " + size)            
+     case (url, (size, links)) => {
+       println("Stats for " + url + ":")
+       println("  size  = "  + size)
+       println("  links = " + links)
+     }     
    }
  }
 }
-// END:concurrent
 
-// START:script
 println("Sequential run:")
 timeMethod { getPageSizeSequentially }
 
 println("Concurrent run")
 timeMethod { getPageSizeConcurrently }
-// END:script
