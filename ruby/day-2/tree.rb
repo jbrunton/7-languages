4423e39f583b3b18fa4cb5f5dba7c4553604da7d
John Brunton <john_brunton@live.co.uk>
2022-05-22 16:48:58 -0400

    accept nested hashes

--- ruby/day-2/tree.rb
+++ ruby/day-2/tree.rb
@@ -1,9 +1,16 @@
 class Tree
   attr_accessor :children, :node_name
   
-  def initialize(name, children=[])
-    @children = children
-    @node_name = name
+  def initialize(hash)
+    if hash.length != 1
+      raise ArgumentError.new("Expected hash with length 1, received: #{hash}")
+    end
+
+    @node_name = hash.keys[0]
+    # iterate over key, value pairs
+    @children = hash[@node_name].to_a.map do |child|
+      Tree.new(child[0] => child[1])
+    end
   end
   
   def visit_all(&block)
@@ -16,9 +23,7 @@ class Tree
   end
 end
 
-ruby_tree = Tree.new( "Ruby", 
-  [Tree.new("Reia"), 
-   Tree.new("MacRuby")] )
+ruby_tree = Tree.new({'grandpa' => { 'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {}, 'child 4' => {} } } })
 
 puts "Visiting a node"
 ruby_tree.visit {|node| puts node.node_name}
